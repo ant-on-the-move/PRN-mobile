cc2e558ae5af6fa567086882aa10ed0fb1e5ac14
ant-on-the-move <antoniodudley92@gmail.com>
2025-06-23 01:45:39 -0400

    add popover

--- app/(main)/(tabs)/_layout.tsx
+++ app/(main)/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from 'expo-router'
 import { Home, MessageCircle, Plus, Bell, Search } from '@tamagui/lucide-icons'
-import { Text, View } from 'tamagui'
+import { Popover, Button, Adapt, YStack, View } from 'tamagui'
 import { TouchableOpacity } from 'react-native'
 
 export default function TabLayout() {
@@ -37,26 +37,62 @@ export default function TabLayout() {
         options={{
           title: '',
           tabBarButton: () => (
-            <TouchableOpacity
-              style={{
-                top: -25,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <View
-                style={{
-                  width: 50,
-                  height: 50,
-                  borderRadius: 25,
-                  backgroundColor: '#0077FF',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
+            <Popover size="$5" allowFlip placement="top">
+              <Popover.Trigger asChild>
+                <TouchableOpacity
+                  style={{
+                    top: -25,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  <View
+                    style={{
+                      width: 50,
+                      height: 50,
+                      borderRadius: 25,
+                      backgroundColor: '#0077FF',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Plus color="white" size={24} />
+                  </View>
+                </TouchableOpacity>
+              </Popover.Trigger>
+
+              <Adapt when="sm" platform="touch">
+                <Popover.Sheet modal dismissOnSnapToBottom>
+                  <Popover.Sheet.Frame p="$4">
+                    <Adapt.Contents />
+                  </Popover.Sheet.Frame>
+                  <Popover.Sheet.Handle />
+                </Popover.Sheet>
+              </Adapt>
+
+              <Popover.Content
+                borderWidth={1}
+                borderColor="$borderColor"
+                enterStyle={{ y: 10, opacity: 0 }}
+                exitStyle={{ y: 10, opacity: 0 }}
+                elevate
+                animation={[
+                  'quick',
+                  {
+                    opacity: {
+                      overshootClamping: true,
+                    },
+                  },
+                ]}
               >
-                <Plus color="white" size={24} />
-              </View>
-            </TouchableOpacity>
+                <Popover.Arrow borderWidth={1} borderColor="$borderColor" />
+                <YStack space="$3" p="$2">
+                  <Button>Create Request</Button>
+                  <Button>Invite Target</Button>
+                  <Button>Add New Target</Button>
+                </YStack>
+              </Popover.Content>
+            </Popover>
           ),
         }}
       />
